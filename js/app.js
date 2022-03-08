a5e373b9ccb26056f9b2983da2fd1ed2db08891f
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-08 09:26:00 +0100

    Solucionado

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-// Implementarsolución aquí
+// Implementar solución aquí
 
+const hexa = "0123456789abcdef";
+
+function randomColor() {
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += hexa.charAt(Math.floor(Math.random() * hexa.length));
+    }
+
+    return color;
+}
+
+document.querySelector("#btn").addEventListener("click", function () {
+    let color = randomColor();
+
+    document.querySelector("#hex-value").textContent = color;
+    document.body.style.backgroundColor = color;
+});
